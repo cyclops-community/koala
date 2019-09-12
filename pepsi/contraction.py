a51d6edbaa298dfdba1f41cb3bf68e64b51dbf6f
Yuchen Pang <32781495+yuchenpang@users.noreply.github.com>
2019-09-12 17:15:33 -0500

    Fix contraction dim order

--- pepsi/contraction.py
+++ pepsi/contraction.py
@@ -1,6 +1,10 @@
+import numpy as np
+
 import peps
 
+
 def contract_peps(grid):
-    nsite = grid.shape[0] * grid.shape[1]
     peps_obj = peps.PEPS(grid, insert_pseudo=False, idx_order=[0,3,2,1,4])
-    return peps_obj.contract().match_virtual().reshape(*[2]*nsite)
+    result = peps_obj.contract().match_virtual().reshape(*[2]*grid.shape[0]*grid.shape[1])
+    result = np.transpose(result, [i+j*grid.shape[0] for i, j in np.ndindex(*grid.shape)])
+    return result
