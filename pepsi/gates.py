b89c85e66e9180c0bcf6e8d2f24d50b28dd33e37
Yuchen Pang <32781495+yuchenpang@users.noreply.github.com>
2019-09-23 15:23:12 -0500

    Add more supported gates

--- pepsi/gates.py
+++ pepsi/gates.py
@@ -39,29 +39,84 @@ def Z(backend):
     return backend.astensor(_Z())
 
 @_register
+@lru_cache(maxsize=None)
+def S(backend):
+    return backend.astensor(_S())
+
+@_register
+@lru_cache(maxsize=None)
+def Sdag(backend):
+    return backend.astensor(_Sdag())
+
+@_register
+@lru_cache(maxsize=None)
+def T(backend):
+    return backend.astensor(_T())
+
+@_register
+@lru_cache(maxsize=None)
+def Tdag(backend):
+    return backend.astensor(_Tdag())
+
+@_register
 @lru_cache(maxsize=64)
 def R(backend, theta):
     return backend.astensor(_R(theta))
 
 @_register
+@lru_cache(maxsize=64)
+def U1(backend, lmbda):
+    return backend.astensor(_U1(lmbda))
+
+@_register
+@lru_cache(maxsize=64)
+def U2(backend, phi, lmbda):
+    return backend.astensor(_U2(phi, lmbda))
+
+@_register
+@lru_cache(maxsize=64)
+def U3(backend, theta, phi, lmbda):
+    return backend.astensor(_U3(theta, phi, lmbda))
+
+@_register
 @lru_cache(maxsize=None)
 def CH(backend):
-    return backend.astensor(_CH())
+    return backend.astensor(_control(1, _H()))
+
+@_register
+@lru_cache(maxsize=None)
+def CX(backend):
+    return backend.astensor(_control(1, _X()))
 
 @_register
 @lru_cache(maxsize=None)
 def CY(backend):
-    return backend.astensor(_CY())
+    return backend.astensor(_control(1, _Y()))
 
 @_register
 @lru_cache(maxsize=None)
 def CZ(backend):
-    return backend.astensor(_CZ())
+    return backend.astensor(_control(1, _Z()))
 
 @_register
 @lru_cache(maxsize=64)
 def CR(backend, theta):
-    return backend.astensor(_CR(theta))
+    return backend.astensor(_control(1, _R(theta)))
+
+@_register
+@lru_cache(maxsize=64)
+def CU1(backend, lmbda):
+    return backend.astensor(_control(1, _U1(lmbda)))
+
+@_register
+@lru_cache(maxsize=64)
+def CU2(backend, phi, lmbda):
+    return backend.astensor(_control(1, _U2(phi, lmbda)))
+
+@_register
+@lru_cache(maxsize=64)
+def CU3(backend, theta, phi, lmbda):
+    return backend.astensor(_control(1, _U3(theta, phi, lmbda)))
 
 @_register
 @lru_cache(maxsize=None)
@@ -75,7 +130,7 @@ def SWAP(backend):
 from itertools import repeat
 import numpy as np
 
-def _add_control(nctrl, tensor):
+def _control(nctrl, tensor):
     nqubit = nctrl + tensor.ndim // 2
     result = np.eye(2**nqubit, dtype=complex)
     result[2**nctrl:, 2**nctrl:] = tensor
@@ -93,25 +148,32 @@ def _Y():
 def _Z():
     return np.array([1,0,0,-1],dtype=complex).reshape(2,2)
 
+def _S():
+    return np.array([1,0,0,1j],dtype=complex).reshape(2,2)
+
+def _Sdag():
+    return np.array([1,0,0,-1j],dtype=complex).reshape(2,2)
+
+def _T():
+    return _U1(np.pi/4)
+
+def _Tdag():
+    return _U1(-np.pi/4)
+
 def _R(theta):
-    return np.array([1,0,0,np.exp(1j*theta)],dtype=complex).reshape(2,2)
+    return _U1(theta)
 
-def _CH():
-    return _add_control(1, _H())
+def _U1(lmbda):
+    return _U3(0, 0, lmbda)
 
-def _CX():
-    return _add_control(1, _X())
+def _U2(phi, lmbda):
+    return _U3(np.pi/2, phi, lmbda)
 
-def _CY():
-    return _add_control(1, _Y())
-
-def _CZ():
-    return _add_control(1, _Z())
-
-def _CR(theta):
-    return _add_control(1, _R(theta))
+def _U3(theta, phi, lmbda):
+    c, s = np.cos(theta), np.sin(theta)
+    e_phi, e_lmbda = np.exp(1j*phi), np.exp(1j*lmbda)
+    return np.array([c,-e_lmbda*s,e_phi*s,e_lmbda*e_phi*c],dtype=complex).reshape(2,2)
 
 def _SWAP():
     return np.array([1,0,0,0,0,0,1,0,0,1,0,0,0,0,0,1],dtype=complex).reshape(2,2,2,2)
 # =============================================================================
-
