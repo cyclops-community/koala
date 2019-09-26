Add unit tests and fix a bug in PEPS amplitude calculation

pepsi/peps/peps.py
@@ -129,8 +129,8 @@ class PEPS:
 
     def get_amplitude(self, bits):
         grid = np.empty_like(self.grid, dtype=object)
-        zero = np.array([0,1], dtype=complex)
-        one = np.array([1,0], dtype=complex)
+        zero = np.array([1,0], dtype=complex)
+        one = np.array([0,1], dtype=complex)
         for i, j in np.ndindex(*self.shape):
             grid[i, j] = self.backend.einsum('ijklx,x->ijkl', self.grid[i,j], one if bits[i,j] else zero)
         return contract_peps_value(grid)

pepsi/peps/quantum_register.py
@@ -45,7 +45,7 @@ class PEPSQuantumRegister(QuantumRegister):
         for tensor, qubits in observable:
             positions = [self._qubit_position(qubit) for qubit in qubits]
             state.apply_operator(self.backend.astensor(tensor), positions)
-        return state.inner(self.state)
+        return np.real_if_close(state.inner(self.state))
 
     def peak(self, qubits, nsamples):
         self.state.peak(qubtis, nsamples)