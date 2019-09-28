Fix error in expectation calculation.
The expectation of sum of observables should be calculated as
sum of the expectation of each observable instead of the expectation
of the tensor prodct of observables.

pepsi/peps/quantum_register.py
@@ -41,11 +41,13 @@ class PEPSQuantumRegister(QuantumRegister):
         return np.abs(self.amplitude(bits))**2
 
     def expectation(self, observable):
-        state = self.state.copy()
+        e = 0
         for tensor, qubits in observable:
+            state = self.state.copy()
             positions = [self._qubit_position(qubit) for qubit in qubits]
             state.apply_operator(self.backend.astensor(tensor), positions)
-        return np.real_if_close(state.inner(self.state))
+            e += np.real_if_close(state.inner(self.state))
+        return e
 
     def peak(self, qubits, nsamples):
         self.state.peak(qubtis, nsamples)

pepsi/statevector/quantum_register.py
@@ -36,14 +36,15 @@ class StateVectorQuantumRegister(QuantumRegister):
         return np.abs(self.amplitude(bits))**2
 
     def expectation(self, observable):
-        state = self.backend.copy(self.state)
+        e = 0
         for tensor, qubits in observable:
+            state = self.backend.copy(self.state)
             apply_operator(self.backend, state, self.backend.astensor(tensor), qubits)
-        expectation_value = self.backend.einsum(
-            state, range(self.nqubit),
-            self.backend.conjugate(self.state), range(self.nqubit),
-        )
-        return np.real_if_close(expectation_value)
+            e += np.real_if_close(self.backend.einsum(
+                state, range(self.nqubit),
+                self.backend.conjugate(self.state), range(self.nqubit),
+            ))
+        return e
 
     def probabilities(self):
         prob_vector = np.real(self.state)**2 + np.imag(self.state)**2