e7ecc6d5cd3d2348b0d29b205967809c5cd20486
Yuchen Pang <32781495+yuchenpang@users.noreply.github.com>
2019-09-30 10:34:16 -0500

    Add tests for expectation calculation

--- test/test_peps.py
+++ test/test_peps.py
@@ -2,7 +2,7 @@ import unittest
 
 import numpy as np
 
-from pepsi import PEPSQuantumRegister
+from pepsi import PEPSQuantumRegister, Observable
 
 from .circuit import Circuit, Gate
 
@@ -32,3 +32,18 @@ class TestPEPS(unittest.TestCase):
         ]))
         self.assertTrue(np.isclose(qreg.probability([1,0,0,1,0,0]), 1/2))
         self.assertTrue(np.isclose(qreg.probability([1,1,0,1,1,0]), 1/2))
+
+    def test_expectation(self):
+        qreg = PEPSQuantumRegister(2, 3, backend='numpy')
+        qreg.apply_circuit(Circuit([
+            Gate('X', [], [0]),
+            Gate('CX', [], [0,3]),
+            Gate('H', [], [2]),
+        ]))
+        observable = Observable.sum([
+            Observable.Z(0),
+            Observable.Z(1),
+            Observable.Z(2),
+            Observable.Z(3),
+        ])
+        self.assertTrue(np.isclose(qreg.expectation(observable), -1))
--- test/test_statevector.py
+++ test/test_statevector.py
@@ -2,7 +2,7 @@ import unittest
 
 import numpy as np
 
-from pepsi import StateVectorQuantumRegister
+from pepsi import StateVectorQuantumRegister, Observable
 
 from .circuit import Circuit, Gate
 
@@ -32,3 +32,18 @@ class TestStateVector(unittest.TestCase):
         ]))
         self.assertTrue(np.isclose(qreg.probability([1,0,0,1,0,0]), 1/2))
         self.assertTrue(np.isclose(qreg.probability([1,1,0,1,1,0]), 1/2))
+
+    def test_expectation(self):
+        qreg = StateVectorQuantumRegister(6, backend='numpy')
+        qreg.apply_circuit(Circuit([
+            Gate('X', [], [0]),
+            Gate('CX', [], [0,3]),
+            Gate('H', [], [2]),
+        ]))
+        observable = Observable.sum([
+            Observable.Z(0),
+            Observable.Z(1),
+            Observable.Z(2),
+            Observable.Z(3),
+        ])
+        self.assertTrue(np.isclose(qreg.expectation(observable), -1))
