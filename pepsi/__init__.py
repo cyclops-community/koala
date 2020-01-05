d89ac18f8c9f26b55cb96bc9e92ba823e787db87
Yiqing Zhou <zhouyiqingkelly@gmail.com>
2020-01-05 16:41:31 +0800

    Rename

--- pepsi/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-PEPSi
-"""
-
-from .version import VERSION as __version__
-
-from .quantum_state import QuantumState
-from .observable import Observable
--- pepsi/statevector/__init__.py
+++
@@ -1,2 +0,0 @@
-from .statevector import StateVector
-from .constructors import computational_zeros, computational_ones, computational_basis
