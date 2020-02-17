f493ac4154a86655563b1407f84939b2dd64d8cd
Yuchen Pang <32781495+yuchenpang@users.noreply.github.com>
2020-02-17 16:17:36 -0600

    Unify PEPS data structure (#7)
    * integrate contraction to main PEPS class
    
    * add contract_snake
    
    * simplify contract_snake and remove redundant files
    
    * Reorganize code
    
    Co-authored-by: Tianyi Hao <haoty0116@gmail.com>

--- koala/peps/constructors.py
+++ koala/peps/constructors.py
@@ -7,7 +7,7 @@ def computational_zeros(nrow, ncol, backend='numpy'):
     backend = tensorbackends.get(backend)
     grid = np.empty((nrow, ncol), dtype=object)
     for i, j in np.ndindex(nrow, ncol):
-        grid[i, j] = backend.astensor(np.array([1,0],dtype=complex).reshape(1,1,1,1,2))
+        grid[i, j] = backend.astensor(np.array([1,0],dtype=complex).reshape(1,1,1,1,2,1))
     return PEPS(grid, backend)
 
 
@@ -15,7 +15,7 @@ def computational_ones(nrow, ncol, backend='numpy'):
     backend = tensorbackends.get(backend)
     grid = np.empty((nrow, ncol), dtype=object)
     for i, j in np.ndindex(nrow, ncol):
-        grid[i, j] = backend.astensor(np.array([0,1],dtype=complex).reshape(1,1,1,1,2))
+        grid[i, j] = backend.astensor(np.array([0,1],dtype=complex).reshape(1,1,1,1,2,1))
     return PEPS(grid, backend)
 
 
@@ -25,6 +25,6 @@ def computational_basis(nrow, ncol, bits, backend='numpy'):
     grid = np.empty_like(bits, dtype=object)
     for i, j in np.ndindex(*bits.shape):
         grid[i, j] = backend.astensor(
-            np.array([0,1] if bits[i,j] else [1,0],dtype=complex).reshape(1,1,1,1,2)
+            np.array([0,1] if bits[i,j] else [1,0],dtype=complex).reshape(1,1,1,1,2,1)
         )
     return PEPS(grid, backend)
---
+++ koala/peps/sites.py
@@ -0,0 +1,57 @@
+"""
+This module implements operations on PEPS sites.
+
+Here are the convetions for names of directions and order of axes.
+
+Direction names:        |   Axes order:
+          z             |            4 0
+          |/            |            |/
+       -- O -- y        |       3 -- O -- 1
+         /|             |           /|
+        x               |          2 5
+"""
+
+import numpy as np
+
+
+def contract_x(a, b):
+    return a.backend.einsum('abidpq,iBcDPQ->a(bB)c(dD)(pP)(qQ)', a, b)
+
+def contract_y(a, b):
+    return a.backend.einsum('aicdpq,AbCiPQ->(aA)b(cC)d(pP)(qQ)', a, b)
+
+def contract_z(a, b):
+    return a.backend.einsum('abcdpi,ABCDiq->(aA)(bB)(cC)(dD)pq', a, b)
+
+
+def reduce_x(a, b):
+    return a.backend.einsumsvd('abidpq,iBcDPQ->abIdpq,IBcDPQ', a, b)
+
+def reduce_y(a, b):
+    return a.backend.einsumsvd('aicdpq,AbCiPQ->aIcdpq,AbCIPQ', a, b)
+
+def reduce_z(a, b):
+    return a.backend.einsumsvd('abcdpi,ABCDiq->abcdpI,ABCDIq', a, b)
+
+
+def rotate_x(a):
+    p = np.roll([4, 1, 5, 3], n)
+    return a.transpose(0, p[1], 2, p[3], p[0], p[2])
+
+def rotate_y(a, n=1):
+    p = np.roll([4, 0, 5, 2], n)
+    return a.transpose(p[2], 1, p[4], 3, p[0], p[3])
+
+def rotate_z(a, n=1):
+    p = np.roll([0, 1, 2, 3], n)
+    return a.transpose(p[0], p[1], p[2], p[3], 4, 5)
+
+
+def flip_x(a):
+    return a.transpose(2, 1, 0, 3, 4, 5)
+
+def flip_y(a):
+    return a.transpose(0, 3, 2, 1, 4, 5)
+
+def flip_z(a):
+    return a.transpose(0, 1, 2, 3, 5, 4)
