e57f9ce50c1b31f94b9bf8d074fa5a6f7dad8af9
Yuchen Pang <32781495+yuchenpang@users.noreply.github.com>
2020-03-13 16:18:34 -0500

    Add gram_qr_local2

--- koala/peps/update.py
+++ koala/peps/update.py
@@ -150,31 +150,33 @@ def apply_local_pair_operator_gram_qr_local(state, operator, positions, rank):
         gram_y_subscripts = 'cfghyq,CfghYq->ycYC'
         xq_subscripts = 'abcdxp,xci->abdpi'
         yq_subscripts = 'cfghyq,ycj->fghqj'
-        recover_x_subscripts = 'abdpi,isu,s->absdup'
-        recover_y_subscripts = 'fghqj,jsv,s->sfghvq'
+        recover_x_subscripts = 'abcdxp,cxsu->absdup'
+        recover_y_subscripts = 'cfghyq,cysv->sfghvq'
     elif x_pos[0] > y_pos[0]: # [y x]^T
         gram_x_subscripts = 'abcdxp,AbcdXp->xaXA'
         gram_y_subscripts = 'efahyq,efAhYq->yaYA'
         xq_subscripts = 'abcdxp,xai->bcdpi'
         yq_subscripts = 'efahyq,yaj->efhqj'
-        recover_x_subscripts = 'bcdpi,isu,s->sbcdup'
-        recover_y_subscripts = 'efhqj,jsv,s->efshvq'
+        recover_x_subscripts = 'abcdxp,axsu->sbcdup'
+        recover_y_subscripts = 'efahyq,aysv->efshvq'
     elif x_pos[1] < y_pos[1]: # [x y]
         gram_x_subscripts = 'abcdxp,aBcdXp->xbXB'
         gram_y_subscripts = 'efgbyq,efgBYq->ybYB'
         xq_subscripts = 'abcdxp,xbi->acdpi'
         yq_subscripts = 'efgbyq,ybj->efgqj'
-        recover_x_subscripts = 'acdpi,isu,s->ascdup'
-        recover_y_subscripts = 'efgqj,jsv,s->efgsvq'
+        recover_x_subscripts = 'abcdxp,bxsu->ascdup'
+        recover_y_subscripts = 'efgbyq,bysv->efgsvq'
     elif x_pos[1] > y_pos[1]: # [y x]
         gram_x_subscripts = 'abcdxp,abcDXp->xdXD'
         gram_y_subscripts = 'edghyq,eDghYq->ydYD'
-        xq_subscripts = 'abcdxp,xci->abcpi'
-        yq_subscripts = 'cfghyq,ycj->eghqj'
-        recover_x_subscripts = 'abcpi,isu,s->abcsup'
-        recover_y_subscripts = 'eghqj,jsv,s->esghvq'
+        xq_subscripts = 'abcdxp,xdi->abcpi'
+        yq_subscripts = 'edghyq,ydj->eghqj'
+        recover_x_subscripts = 'abcdxp,dxsu->abcsup'
+        recover_y_subscripts = 'edghyq,dysv->esghvq'
     else:
         assert False
+
+    numpy_backend = tensorbackends.get('numpy')
 
     def gram_qr_local(backend, a, gram_a_subscripts, q_subscripts):
         gram_a = backend.einsum(gram_a_subscripts, a.conj(), a)
@@ -182,7 +184,7 @@ def apply_local_pair_operator_gram_qr_local(state, operator, positions, rank):
 
         # local
         with Timer(state.backend, 'KOALA_gram_qr_local'):
-            with Timer(state.backend, 'KOALA_ctf_to_numpy'):
+            with Timer(state.backend, 'KOALA_gram_a_to_numpy'):
                 gram_a = gram_a.numpy().reshape(d*xi, d*xi)
             with Timer(state.backend, 'KOALA_eigh_etc'):
                 w, v = la.eigh(gram_a, overwrite_a=True)
@@ -190,20 +192,25 @@ def apply_local_pair_operator_gram_qr_local(state, operator, positions, rank):
                 s_pinv = np.divide(1, s, out=np.zeros_like(s), where=s!=0)
                 r = np.einsum('j,ij->ji', s, v.conj()).reshape(d*xi, d, xi)
                 r_inv = np.einsum('j,ij->ij', s_pinv, v).reshape(d, xi, d*xi)
-
-        with Timer(state.backend, 'KOALA_numpy_to_ctf'):
-            r = backend.astensor(r)
-            r_inv = backend.astensor(r_inv)
-        with Timer(state.backend, 'KOALA_multiply_r_inv'):
-            q = backend.einsum(q_subscripts, a, r_inv)
-        return q, r
+        return numpy_backend.tensor(r), numpy_backend.tensor(r_inv)
 
     with Timer(state.backend, 'KOALA_gram_qr'):
-        xq, xr = gram_qr_local(state.backend, x, gram_x_subscripts, xq_subscripts)
-        yq, yr = gram_qr_local(state.backend, y, gram_y_subscripts, yq_subscripts)
-    with Timer(state.backend, 'KOALA_einsumsvd'):
-        u, s, v = state.backend.einsumsvd('ixk,jyk,xyuv->isu,jsv', xr, yr, operator, option=ReducedSVD(rank))
+        xr, xr_inv = gram_qr_local(state.backend, x, gram_x_subscripts, xq_subscripts)
+        yr, yr_inv = gram_qr_local(state.backend, y, gram_y_subscripts, yq_subscripts)
+
+    with Timer(state.backend, 'KOALA_operator_to_numpy'):
+        operator = numpy_backend.tensor(operator.numpy())
+
+    with Timer(state.backend, 'KOALA_einsumsvd_local'):
+        u, s, v = numpy_backend.einsumsvd('ixk,jyk,xyuv->isu,jsv', xr, yr, operator, option=ReducedSVD(rank))
+        s **= 0.5
+        u = numpy_backend.einsum('xki,isu,s->kxsu', xr_inv, u, s)
+        v = numpy_backend.einsum('ykj,jsv,s->kysv', yr_inv, v, s)
+
+    with Timer(state.backend, 'KOALA_uv_to_ctf'):
+        u = state.backend.astensor(u)
+        v = state.backend.astensor(v)
+
     with Timer(state.backend, 'KOALA_recover_site'):
-        s = s ** 0.5
-        state.grid[x_pos] = state.backend.einsum(recover_x_subscripts, xq, u, s)
-        state.grid[y_pos] = state.backend.einsum(recover_y_subscripts, yq, v, s)
+        state.grid[x_pos] = state.backend.einsum(recover_x_subscripts, x, u)
+        state.grid[y_pos] = state.backend.einsum(recover_y_subscripts, y, v)
