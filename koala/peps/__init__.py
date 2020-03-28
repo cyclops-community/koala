0baec8e0ef17b9e9c5c316c0a4326666e51d6b66
Yuchen Pang <32781495+yuchenpang@users.noreply.github.com>
2020-03-28 14:35:07 -0500

    Add save/load for peps

--- koala/peps/__init__.py
+++ koala/peps/__init__.py
@@ -1,4 +1,4 @@
-from .peps import PEPS, braket
+from .peps import PEPS, braket, save, load
 from .constructors import computational_zeros, computational_ones, computational_basis, random
 from .contraction import Snake, ABMPS, BMPS, SingleLayer, Square, TRG, contract_options
 from .update import DirectUpdate, QRUpdate, LocalGramQRUpdate, LocalGramQRSVDUpdate, DefaultUpdate
